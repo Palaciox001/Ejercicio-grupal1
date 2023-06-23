6250381e798499117486a6bc33d32b968f67fe62
Palaciox001 <palaciossanzd11@gmail.com>
2023-06-23 09:25:15 +0200

    first commit

---
+++ Teammembers.js
@@ -0,0 +1,14 @@
+const teamMembers = [
+    {
+      firstName: 'Daniel',
+      surname: 'Palacios',
+      age: '21',
+      city: 'Madrid',
+      hobby: 'Crossfit',
+      favoriteFood: 'Croquetas',
+      favoriteVideoGame: 'Pokemon Perla',
+      favoriteFilm: 'Pretty Woman',
+      favoriteBook: 'Endgame',
+      petName: 'Sora',
+    },
+  ];
