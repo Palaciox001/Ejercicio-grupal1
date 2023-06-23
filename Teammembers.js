4dd3729de1768d6210ce6dbe166e95dc503877fa
Jeniffer <hachikofu03@gmail.com>
2023-06-23 09:33:45 +0200

    updated

--- Teammembers.js
+++ Teammembers.js
@@ -11,4 +11,16 @@ const teamMembers = [
       favoriteBook: 'Endgame',
       petName: 'Sora',
     },
+    {
+      firstName: 'Jeniffer',
+      surname: 'Flores',
+      age: '20',
+      city: 'Murcia',
+      hobby: 'Draw',
+      favoriteFood: 'Tortilla de patatas',
+      favoriteVideoGame: 'Valorant',
+      favoriteFilm: 'Ready Player Number One',
+      favoriteBook: 'Deja en paz a los muertos',
+      petName: 'Nube',
+    }
   ];
